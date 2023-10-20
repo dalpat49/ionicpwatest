830bfa10a99a5a530acfc234f8c630ea793085bb
Dalpat singh <singhdalpat8182@gmail.com>
2023-10-20 18:03:41 +0530

    new ap

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,17 @@
+
+<script lang="ts" setup>
+import { IonContent, IonHeader, IonFooter, IonButtons, IonButton, IonPage, IonTitle, IonToolbar, IonFab,IonFabButton,IonIcon, actionSheetController, isPlatform } from '@ionic/vue';
+  
+const openALAERT  =()=>{
+  alert('hi')
+}
+
+const  alertButtons = ['ok done']
+
+</script>
+
+
+
 <template>
   <ion-page>
     <ion-header :translucent="true">
@@ -16,46 +30,27 @@
         </ion-buttons>
       </ion-toolbar>
     </ion-header>
+
     
     <ion-content :fullscreen="true">
+      <ion-button  @click="openALAERT">Disabled</ion-button>
+      
+
+    <ion-button id="present-alert">Click Me</ion-button>
+      <ion-alert
+        trigger="present-alert"
+        header="Alert"
+        sub-header="Important message"
+        message="This is an alert!"
+        :buttons="alertButtons"
+      ></ion-alert>
+
+
+
     </ion-content>
-    <ion-footer >
-      <ion-toolbar>
-        <ion-buttons>
-          <ion-button>
-            Select All
-          </ion-button>
-          <ion-button >
-            Deselct All
-          </ion-button>
-          <ion-button>
-            Remove Selected
-          </ion-button>
-        </ion-buttons>
-      </ion-toolbar>
-    </ion-footer>
+   
   </ion-page>
 </template>
-
-<script lang="ts" setup>
-import { IonContent, IonHeader, IonFooter, IonButtons, IonButton, IonPage, IonTitle, IonToolbar, IonFab,IonFabButton,IonIcon, actionSheetController, isPlatform } from '@ionic/vue';
-import Dynamsoft from 'mobile-web-capture';
-import { WebTwain } from 'mobile-web-capture/dist/types/WebTwain';
-import { DocumentConfiguration } from "mobile-web-capture/dist/types/Addon.Camera";
-import { Base64Result } from "mobile-web-capture/dist/types/WebTwain.IO";
-import { defineComponent, onMounted, ref } from 'vue';
-import DWT from '../components/DWT.vue'
-import { createOutline,cameraOutline,saveOutline,checkboxOutline,checkbox,ellipsisHorizontal,ellipsisVertical } from 'ionicons/icons';
-import { addIcons } from 'ionicons';
-import { ThumbnailViewer } from 'mobile-web-capture/dist/types/WebTwain.Viewer';
-import { AndroidPermissions } from '@awesome-cordova-plugins/android-permissions/';
-import { Directory, Filesystem } from '@capacitor/filesystem';
-import { Capacitor } from "@capacitor/core";
-import { Share } from '@capacitor/share';
-import { DynamsoftEnumsDWT } from 'mobile-web-capture/dist/types/Dynamsoft.Enum';
-
-  
-</script>
 
 <style scoped>
 ion-buttons {
